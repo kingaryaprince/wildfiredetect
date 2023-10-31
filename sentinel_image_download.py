226e7f09a098bb8f7a75cbdeca0a2609b514ff56
kingaryaprince <103230082+kingaryaprince@users.noreply.github.com>
2023-10-30 22:04:35 -0700

    Update sentinel_image_download.py
    Added docstring

--- sentinel_image_download.py
+++ sentinel_image_download.py
@@ -37,7 +37,25 @@ def mime2ext(image_format):
 
 def process_row(config, row, out_dir, buffer_size=0.1, img_size=None, layer='TRUECOLOR', max_cc=0.3,
                 image_format=MimeType.JPG, data_collection=DataCollection.SENTINEL2_L2A):
-    """ Process individual row from dataframe """
+    """
+    Process a given row of data (that is extracted from VIIRS fire data), and extract available images 
+    based on specified parameters such as longitude, latitude, timeinterval.
+
+    Parameters:
+    - config (SHConfig): Configuration object for the Sentinel Hub request.
+    - row (pd.Series): A single row from the dataframe containing columns such as 'latitude', 'longitude', 'fire_date', etc.
+    - out_dir (str): Directory where the downloaded images will be saved.
+    - buffer_size (float, optional): Buffer size for bounding box calculation around latitude and longitude. Defaults to 0.1.
+    - img_size (tuple, optional): Image dimensions as (width, height). If not provided, default values will be derived.
+    - layer (str, optional): Layer specification for the Sentinel Hub request. Defaults to 'TRUECOLOR'.
+    - max_cc (float, optional): Maximum cloud coverage in fraction (0 to 1). Defaults to 0.3.
+    - image_format (MimeType, optional): Format for the downloaded image. Defaults to MimeType.JPG.
+    - data_collection (DataCollection, optional): Data collection source. Defaults to DataCollection.SENTINEL2_L2A.
+
+    Returns:
+    None
+
+    """
     latitude, longitude, fire_date, adj_start_date, adj_end_date = extract_row_data(row)
 
     bbox = convert2bbox(longitude=longitude, latitude=latitude, buffer_size=buffer_size)
@@ -93,7 +111,40 @@ def process_row(config, row, out_dir, buffer_size=0.1, img_size=None, layer='TRU
 def download_layer(config, data_collection, layer, csv_in_dir, base_out_dir, buffer_size, img_size,
                    start_date, end_date, backward_days, forward_days, max_rows, rand_seed,
                    longitude=None, latitude=None, image_format=MimeType.JPG, fire_confidence=['h', 'high', 'n', 'nominal']):
-    """ Download specific layers """
+    
+    """
+    Extract VIIRS fire data from previously saved csv files based on certain specific parameters, 
+    and download corresponding to satellite imagery.
+
+    Args:
+    - config (SHConfig): Configuration object for the SentinelHub service.
+    - data_collection (DataCollection): Specifies the data source collection (e.g., SENTINEL2_L2A).
+    - layer (str): Name of the Layer configured in SentinelHub Configurator, for the specific instance (e.g. TRUECOLOR, FIREMASK) .
+    - csv_in_dir (str): Path to the input directory containing VIIRS fire data CSV files.
+    - base_out_dir (str): Base directory where the downloaded images will be saved.
+    - buffer_size (float): Buffer size used when calculating bounding box from a point coordinate.
+    - img_size (tuple): Tuple (width, height) representing the desired size of the downloaded image.
+    - start_date (str): The earliest acquisition date to consider when filtering data from the CSV files (in 'YYYY-MM-DD' format).
+    - end_date (str): The latest acquisition date to consider when filtering data from the CSV files (in 'YYYY-MM-DD' format).
+    - backward_days (int): Number of days to look backwards from acquisition date for the time interval when downloading image.
+    - forward_days (int): Number of days to look forward from acquisition date for the time interval when downloading image.
+    - max_rows (int): Maximum number of rows (data points) to process.
+    - rand_seed (int): Seed for random number generation, to randomly extact the rows from the input CSV files.
+    - longitude (float, optional): Specific longitude coordinate to narrow down the data preparation. Default (None) means no limit 
+    - latitude (float, optional): Specific latitude coordinate to narrow down the data preparation. Default (None) means no limit
+    - image_format (MimeType, optional): Desired format of the downloaded image. Defaults to JPG.
+    - fire_confidence (list, optional): List of fire confidence levels to filter the data. Defaults to ['h', 'high', 'n', 'nominal'].
+
+    Returns:
+    - None
+
+    Output:
+    - Downloads and saves images to a folder under `base_out_dir`.
+
+    Notes:
+    - If the resultant DataFrame after data preparation is empty, the function logs an error and does nothing further.
+    - Between processing each row (or downloading each image), a random sleep interval is added for throttling.
+    """
     project_name = data_collection.name[-6:]
     out_dir = os.path.join(base_out_dir, project_name)
     os.makedirs(out_dir, exist_ok=True)
@@ -128,6 +179,6 @@ if __name__ == "__main__":
 
     download_layer(config=config, data_collection=DataCollection.SENTINEL2_L2A, layer='TRUECOLOR',
                    csv_in_dir=r"C:\wildfire\data\csv\recent", base_out_dir=r"C:\wildfire\data\images",
-                   buffer_size=0.05, img_size=(350, 350), start_date='2016-01-01', end_date='2023-10-30',
+                   buffer_size=0.05, img_size=(350, 350), start_date='2023-01-01', end_date='2023-10-30',
                    backward_days=0, forward_days=1, max_rows=1000, rand_seed=13,
                    fire_confidence=['h', 'high', 'n', 'nominal'])
